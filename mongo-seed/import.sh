efbd87e6cc872341da8c3cb2109e4193648857d6
Jannik Brozy <jo@pop-os.localdomain>
2023-03-18 20:36:37 +0100

    fixed import

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -6,4 +6,13 @@
 
 # mongoimport --host localhost --port 27017 --db mondial --authenticationDatabase mondial --username mondial --password swordfish --collection geo_objects --type json --file /geo_objects.json --jsonArray
 
-mongoimport --collection organizations --drop --file /data/organizations.json
+mongoimport --host mongodb -u mondial -p swordfish --collection organizations --db mondial --file ./organizations.json --authenticationDatabase=admin --type json --jsonArray --drop
+
+mongoimport --host mongodb -u mondial -p swordfish --collection cities --db mondial --file ./cities.json --authenticationDatabase=admin --type json --jsonArray --drop
+
+mongoimport --host mongodb -u mondial -p swordfish --collection provinces --db mondial --file ./provinces.json --authenticationDatabase=admin --type json --jsonArray --drop
+
+mongoimport --host mongodb -u mondial -p swordfish --collection geo_objects --db mondial --file ./geo_objects.json --authenticationDatabase=admin --type json --jsonArray --drop
+
+mongoimport --host mongodb -u mondial -p swordfish --collection countries --db mondial --file ./countries.json --authenticationDatabase=admin --type json --jsonArray --drop
+
